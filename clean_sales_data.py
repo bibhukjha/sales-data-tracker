b0a3ab930deec9eb058c8560bb3cb9a93fa20cad
give your user name here <msgbox.ubibhu@gmail.com>
2025-05-24 13:31:20 +0530

    add sales data and clean script

---
+++ clean_sales_data.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+# Read the sales CSV
+df = pd.read_csv('sales_data.csv')
+
+# Print original data
+print("Original Data:\n", df)
+
+# Drop rows with missing values
+cleaned_df = df.dropna()
+
+# Save cleaned data
+cleaned_df.to_csv('cleaned_sales_data.csv', index=False)
+
+print("\nCleaned Data:\n", cleaned_df)
+
